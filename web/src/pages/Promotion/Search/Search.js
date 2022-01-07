6c40b68e69f1b6a13960448826307168653f8f06
Jones-bass <jonesbass_tb@gmail.com>
2022-01-07 11:56:25 -0300

    7 Aula React

--- web/src/pages/Promotion/Search/Search.js
+++ web/src/pages/Promotion/Search/Search.js
@@ -1,27 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import PromotionCard from 'components/Promotion/Card/Card';
-import axios from 'axios';
+import React from 'react';
+import UIContainer from 'components/UI/Container';
+import PromotionSearch from 'components/Promotion/Search/Search';
+
 
 const PagesPromotionSearch = () => {
-  const [promotions, setPromotions] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/promotions?_embed=comments")
-      .then((response) => {
-        setPromotions(response.data);
-      });
-  }, []);
 
   return (
-    <div
-      style={{
-        maxWidth: 800,
-        margin: '30px auto',
-      }}
-    >
-      {promotions.map((promotion) => (
-        <PromotionCard promotion={promotion} />
-      ))};
+    <div>
+      <UIContainer>
+        <PromotionSearch />
+      </UIContainer>
     </div>
   );
 };
